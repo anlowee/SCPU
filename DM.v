886aaa797fd047972108289a527982daf7ea4b1c
Eddy Wei <anlowee@outlook.com>
2020-04-30 17:54:01 +0800

    :construction:
    完成DM、IM

--- DM.v
+++ DM.v
@@ -1,1 +1,18 @@
+module DM(clk, Addr, DMWr, DataIn, DataOut);  // 8-bits PC, not 32 bits
+
+    input clk;
+    input DMWr;
+    input   [7:0] Addr;
+    input   [31:0] DataIn;
+    output  [31:0] DataOut;
+
+    reg [31:0] DataMem[253:0];
+   
+   always @(posedge clk) begin
+      if (DMWr) DataMem[Addr] <= DataIn;
+   end
+
+   assign DataOut = DataMem[Addr];
+
+endmodule
 
--- IM.v
+++ IM.v
@@ -1,1 +1,10 @@
+module IM(PC, Instruction);  // 8-bits PC, not 32 bits
 
+    input   [7:0] PC;
+    output  [31:0] Instruction;
+
+    reg [31:0] InstrctionMem[253:0];
+
+    assign Instruction = InstrctionMem[PC];
+
+endmodule
