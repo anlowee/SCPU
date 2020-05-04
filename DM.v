2eefbe9385fbd981965b452c7b7e75b640dce6aa
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-04 23:15:39 +0800

    Update DM.v
    refract to litte dian

--- DM.v
+++ DM.v
@@ -14,20 +14,23 @@ module DM(clk, Addr, DMRe, DMWr, DataIn, DataOut);  // 8-bits PC, not 32 bits
    always @(posedge clk) begin
       case (DMWr)
          `DMWR_SW:   begin
-            DataMem[Addr] <= DataIn[31:24];
-            DataMem[Addr + 1] <= DataIn[23:16];
-            DataMem[Addr + 2] <= DataIn[15:8];
-            DataMem[Addr + 3] <= DataIn[7:0];
+            DataMem[Addr] <= DataIn[7:0];
+            DataMem[Addr + 1] <= DataIn[15:8];
+            DataMem[Addr + 2] <= DataIn[23:16];
+            DataMem[Addr + 3] <= DataIn[31:24];
          end
          `DMWR_SH:   begin
-            DataMem[Addr] <= DataIn[31:24];
-            DataMem[Addr] <= DataIn[23:16];
+            DataMem[Addr] <= DataIn[7:0];
+            DataMem[Addr + 1] <= DataIn[15:8];
          end
          `DMWR_SB:   begin
-            DataMem[Addr] <= DataIn[31:24];
+            DataMem[Addr] <= DataIn[7:0];
+         end
+         `DMWR_NOP:  begin
+            DataMem[Addr] <= DataMem[Addr];
          end
          default:    begin
-            DataMem[Addr] <= DataIn[31:24];
+            DataMem[Addr] <= DataMem[Addr];
          end
       endcase
    end
@@ -35,19 +38,22 @@ module DM(clk, Addr, DMRe, DMWr, DataIn, DataOut);  // 8-bits PC, not 32 bits
    always @(DMRe, Addr) begin
       case (DMRe) 
          `DMRE_LW:   begin
-            DataOut <= {DataMem[Addr], DataMem[Addr + 1], DataMem[Addr + 2], DataMem[Addr + 3]};
+            DataOut <= {DataMem[Addr + 3], DataMem[Addr + 2], DataMem[Addr + 1], DataMem[Addr]};
          end
          `DMRE_LH:   begin
-            DataOut <= {{16{DataMem[Addr][7]}}, DataMem[Addr], DataMem[Addr + 1]};
+            DataOut <= {{16{DataMem[Addr + 1][7]}}, DataMem[Addr + 1], DataMem[Addr]};
          end
          `DMRE_LHU:  begin
-            DataOut <= {16'b0, DataMem[Addr], DataMem[Addr + 1]};
+            DataOut <= {16'b0, DataMem[Addr + 1], DataMem[Addr]};
          end
          `DMRE_LB:   begin
             DataOut <= {{24{DataMem[Addr][7]}}, DataMem[Addr]};
          end
          `DMRE_LBU:  begin
             DataOut <= {24'b0, DataMem[Addr]};
+         end
+         `DMRE_NOP:  begin
+            DataOut <= 32'b0;
          end
          default:    begin
             DataOut <= 32'b0;
