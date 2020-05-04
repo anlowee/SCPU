2ce6abb4cae2711f4cdde4db6c44a7e20f6ad3b8
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-04 16:24:27 +0800

    Update EXT.v
    add a new extend tool for 5-bits shamt.

--- EXT.v
+++ EXT.v
@@ -8,3 +8,14 @@ module EXT( Imm16, EXTOp, Imm32 );
    assign Imm32 = (EXTOp) ? {{16{Imm16[15]}}, Imm16} : {16'd0, Imm16}; // signed-extension or zero extension
        
 endmodule
+
+
+module EXT_Shamt( Imm5, EXTOp, Imm32 );
+    
+   input  [4:0] Imm5;
+   input         EXTOp;
+   output [31:0] Imm32;
+   
+   assign Imm32 = (EXTOp) ? {{27{Imm5[4]}}, Imm5} : {27'd0, Imm5}; // signed-extension or zero extension
+       
+endmodule
