46959be4380f95c964181167a491a05f991d7c01
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-04 12:35:34 +0800

    :construction:
    now DM support SW/SH/... and LW/LH/LHU/... operations.

--- DM.v
+++ DM.v
@@ -1,18 +1,59 @@
-module DM(clk, Addr, DMWr, DataIn, DataOut);  // 8-bits PC, not 32 bits
+`include "ctrl_encode_def.v"
+
+module DM(clk, Addr, DMRe, DMWr, DataIn, DataOut);  // 8-bits PC, not 32 bits
 
     input clk;
-    input DMWr;
+    input   [1:0] DMWr;
+    input   [2:0] DMRe;
     input   [7:0] Addr;
     input   [31:0] DataIn;
-    output  [31:0] DataOut;
+    output reg [31:0] DataOut;
 
-    reg [31:0] DataMem[253:0];
+    reg [7:0] DataMem[1023:0];
    
    always @(posedge clk) begin
-      if (DMWr) DataMem[Addr] <= DataIn;
+      case (DMWr)
+         `DMWR_SW:   begin
+            DataMem[Addr] <= DataIn[31:24];
+            DataMem[Addr + 1] <= DataIn[23:16];
+            DataMem[Addr + 2] <= DataIn[15:8];
+            DataMem[Addr + 3] <= DataIn[7:0];
+         end
+         `DMWR_SH:   begin
+            DataMem[Addr] <= DataIn[31:24];
+            DataMem[Addr] <= DataIn[23:16];
+         end
+         `DMWR_SB:   begin
+            DataMem[Addr] <= DataIn[31:24];
+         end
+         default:    begin
+            DataMem[Addr] <= DataIn[31:24];
+         end
+      endcase
    end
 
-   assign DataOut = DataMem[Addr];
+   always @(DMRe, Addr) begin
+      case (DMRe) 
+         `DMRE_LW:   begin
+            DataOut <= {DataMem[Addr], DataMem[Addr + 1], DataMem[Addr + 2], DataMem[Addr + 3]};
+         end
+         `DMRE_LH:   begin
+            DataOut <= {{16{DataMem[Addr][7]}}, DataMem[Addr], DataMem[Addr + 1]};
+         end
+         `DMRE_LHU:  begin
+            DataOut <= {16'b0, DataMem[Addr], DataMem[Addr + 1]};
+         end
+         `DMRE_LB:   begin
+            DataOut <= {{24{DataMem[Addr][7]}}, DataMem[Addr]};
+         end
+         `DMRE_LBU:  begin
+            DataOut <= {24'b0, DataMem[Addr]};
+         end
+         default:    begin
+            DataOut <= 32'b0;
+         end
+      endcase
+   end
 
 endmodule
 
