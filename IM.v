1fe3b21757431b40463732349a2539803cbb35bd
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-04 20:37:53 +0800

    Update IM.v
    fix Instructions set read bug, Instructions must store as Byte.

--- IM.v
+++ IM.v
@@ -3,8 +3,24 @@ module IM(PC, Instruction);  // 8-bits PC, not 32 bits
     input   [7:0] PC;
     output  [31:0] Instruction;
 
-    reg [31:0] InstrctionMem[253:0];
+    reg [7:0] InstrctionMem[1023:0];
+    reg [31:0] InstrctionMemReadTemp[253:0];
+    reg [31:0] curIns;
+    integer i, Addr;
 
-    assign Instruction = InstrctionMem[PC];
+    initial begin
+        Addr = 32'b0;
+        $readmemh("D:\\GitHub\\SCPU\\test.dat", InstrctionMemReadTemp);
+        for (i = 0; i < 254; i = i + 1) begin
+            curIns = InstrctionMemReadTemp[i];
+            InstrctionMem[Addr] = curIns[31:24];
+            InstrctionMem[Addr + 1] = curIns[23:16];
+            InstrctionMem[Addr + 2] = curIns[15:8];
+            InstrctionMem[Addr + 3] = curIns[7:0];
+            Addr = Addr + 4;
+        end
+    end
+
+    assign Instruction = {InstrctionMem[PC], InstrctionMem[PC + 1], InstrctionMem[PC + 2], InstrctionMem[PC + 3]};
 
 endmodule
